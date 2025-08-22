38b6fd713a919312a720453a748edd7109a90ecb
Chandra Sekhar Miriyala <miriyalachandrasekharnaidu@gmail.com>
2025-08-22 11:29:05 +0530

    update

--- backend/models/RoleScope.js
+++ backend/models/RoleScope.js
@@ -2,7 +2,7 @@ const { DataTypes } = require('sequelize');
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgresql://localhost:5432/sri_chakra_diary', {
+const sequelize = new Sequelize('postgresql://postgres:S3@@@1303@db.yrakjnonabrqyicyvdam.supabase.co:5432/postgres', {
   dialect: 'postgres',
   logging: process.env.NODE_ENV === 'development' ? console.log : false,
   pool: {
